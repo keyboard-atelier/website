e64e9a92c68e21aff68c820cb2af7fc35b796c74
ai03-2725 <admin@ai03.com>
2024-02-19 01:17:03 +0900

    Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs({
-    entrypoint: '/src/alpine-entrypoint'
-  }), icon()]
+  site: 'https://kbatelier.org',
+  integrations: [
+    tailwind(), 
+    alpinejs({entrypoint: '/src/alpine-entrypoint'}), 
+    icon(), 
+    sitemap()
+  ]
 });
